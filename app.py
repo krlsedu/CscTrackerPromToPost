56cceb31470b406af100152647f85667012a7722
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2023-09-18 14:25:51 -0300

    Add authorization headers and improve metric handling in app.py
    Implemented usage of authorization headers for API calls and improved the handling of metrics data. This included an option to provide an alternate 'step_dest' value, the integration of converted metrics, and a more efficient start time calculation. These changes will result in more secure API calls and more precise and efficient data gathering.

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ url_prometeus = os.environ['URL_PROMETHEUS']
 @app.route('/convert', methods=['GET'])
 @cross_origin()
 def convert():
+    headers = {
+        'Authorization': "Bearer " + os.environ['TOKEN_INTEGRACAO']
+    }
     args = request.args
     args_ = {}
     args_to_prometheus = ['start', 'end', 'step', 'query', 'timeout', 'dedup', 'partial_response', 'silence']
@@ -30,11 +33,21 @@ def convert():
     if 'step' not in args_:
         args_['step'] = 60
 
-    timestamp_ = int(datetime.now().timestamp() / int(args_['step'])) * int(args_['step'])
+    if 'step_dest' in args:
+        step_dest = args['step_dest']
+    else:
+        step_dest = args_['step']
+
+    timestamp_ = int(datetime.now().timestamp() / int(step_dest)) * int(step_dest)
 
     if 'date' in args:
         date_end = datetime.strptime(args['date'], '%Y-%m-%d %H:%M:%S.%f')
-        timestamp_ = int(date_end.timestamp() / int(args_['step'])) * int(args_['step'])
+        timestamp_ = int(date_end.timestamp() / int(step_dest)) * int(step_dest)
+    converted_metric_ = get_date_end(args_['query'], headers)
+    print(converted_metric_)
+    if 'auto' in args:
+        if converted_metric_ is not None:
+            timestamp_ = converted_metric_['timestamp_start']
 
     if 'end' not in args_:
         args_['end'] = timestamp_
@@ -54,18 +67,21 @@ def convert():
             args_['start'] = timestamp_ - (int(range_) * 60)
 
     if 'start' not in args_:
-        args_['start'] = timestamp_ - (60 * 10)
-
+        args_['start'] = timestamp_ - (11000 * int(args_['step']))
 
     body = None
     response = requests.get(url_prometeus, json=body, params=args_)
 
-    headers = {
-        'Authorization': "Bearer " + os.environ['TOKEN_INTEGRACAO']
-    }
-
     body = convert_response_to_metrics(response, headers)
     response = requests.post(f"{url_repository}metrics?metric&date", headers=headers, json=body)
+    if converted_metric_ is None:
+        converted_metric_ = {'query': args_['query'], 'timestamp_start': args_['start'], 'timestamp_end': args_['end']}
+    else:
+        converted_metric_['timestamp_start'] = args_['start']
+        converted_metric_['timestamp_end'] = args_['end']
+
+    response_ = requests.post(f"{url_repository}converted_metrics", headers=headers, json=converted_metric_)
+    print(response_.json())
     return response.json(), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 
@@ -78,9 +94,9 @@ def convert_response_to_metrics(response, headers=None):
         metric_name = json.dumps(metric['metric'])
         values_ = metric['values']
         last_metric = None
+        values_.sort(key=lambda x: x[0])
         for value in values_:
             count_ += 1
-            print(f"processing {count_} of {len(values_) * len(result)}")
             metric_ = {'metric': metric_name, 'value': str(value[1])}
             timestamp = int(value[0])
             dt_obj = datetime.fromtimestamp(timestamp)
@@ -89,9 +105,17 @@ def convert_response_to_metrics(response, headers=None):
             if last_metric is None or last_metric['value'] != metric_['value']:
                 metrics.append(metric_)
                 last_metric = metric_
-            else:
-                print(f"metric ignored -> {metric_}")
     return metrics
+
+
+def get_date_end(query, headers=None):
+    select_ = (f"select * from converted_metrics cm "
+               f"where cm.query = '{query}' order by timestamp_start limit 1")
+    body = {'command': select_}
+    response = requests.post(f"{url_repository}command/select", headers=headers, json=body)
+    json_ = response.json()
+    if len(json_) > 0:
+        return json_[0]
 
 
 if __name__ == '__main__':
